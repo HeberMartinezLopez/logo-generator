b1bff8460ba0a20d147a6592da765b846f2561e0
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-11-15 17:39:01 -0800

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const SVG = require('./lib/svg');
+const Circle = require('./lib/circle');
+const Square = require('./lib/square');
+const Triangle = require('./lib/triangle');
 
 function makeLogo(){
     inquirer
@@ -28,6 +32,33 @@ function makeLogo(){
             },
         ]).then((res) => {
             console.log(res);
+            let shapeLogo;
+             switch(res.shape){
+                case "Circle": 
+                 shapeLogo = new Circle()
+                 break;
+
+                 case "Square": 
+                 shapeLogo = new Square()
+                 break;
+                 
+                 case "Triangle": 
+                 shapeLogo = new Triangle()
+                 break;
+             }
+             shapeLogo.setColor(res.backgroundColor);
+             const newLogo = new SVG ()
+             newLogo.setShape(shapeLogo)
+             newLogo.setText(res.textcolor,res.text)
+             if(res.text.length > 3){
+                console.log('Only 3 characters allowed!')
+                makeLogo()
+        
+             }
+             renderMyLogo('logo.svg', newLogo.render());
         })
+}
+function renderMyLogo(filename, res){
+    fs.writeFileSync(filename, res)
 }
 makeLogo();
